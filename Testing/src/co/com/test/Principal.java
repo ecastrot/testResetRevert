e7e1d5ded3d4f357e4cb381eb02e464d50fbd625
Edison Castro <ecastrot@loggro.com>
2021-08-03 10:16:44 -0500

    Cosa 3 paso en master

--- Testing/src/co/com/test/Principal.java
+++ Testing/src/co/com/test/Principal.java
@@ -12,5 +12,6 @@ public class Principal {
 		System.out.println("Cambio 2 en 4");
 		System.out.println("Cosa 1 paso en master");
 		System.out.println("Cosa 2 paso en master");
+		System.out.println("Cosa 3 paso en master");
 	}
 }
