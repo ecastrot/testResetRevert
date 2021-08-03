b4009382868ed8933720ba73db422e6aba3e4554
Edison Castro <ecastrot@loggro.com>
2021-08-03 10:22:04 -0500

    Cambio 2 paso en cinco

--- Testing/src/co/com/test/Principal.java
+++ Testing/src/co/com/test/Principal.java
@@ -12,5 +12,6 @@ public class Principal {
 		System.out.println("Cosa 2 paso en master");
 		System.out.println("Cosa 3 paso en master");
 		System.out.println("Cambio 1 paso en cinco");
+		System.out.println("Cambio 2 paso en cinco");
 	}
 }
