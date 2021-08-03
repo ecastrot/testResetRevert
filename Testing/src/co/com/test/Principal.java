82b06935c608651f79d64c0c92b2db743db7d371
Edison Castro <ecastrot@loggro.com>
2021-08-03 09:18:21 -0500

    Sexto cambio en branch[third]

--- Testing/src/co/com/test/Principal.java
+++ Testing/src/co/com/test/Principal.java
@@ -8,5 +8,6 @@ public class Principal {
 		System.out.println("Tercer cambio en branch[second]");
 		System.out.println("Cuarto cambio en branch[second]");
 		System.out.println("Quinto cambio en branch[third]");
+		System.out.println("Sexto cambio en branch[third]");
 	}
 }
