80fb568626ca66ec9792f51126a376c2318dcd2b
Edison Castro <ecastrot@loggro.com>
2021-08-03 09:07:30 -0500

    Tercer cambio en branch[second]

--- Testing/src/co/com/test/Principal.java
+++ Testing/src/co/com/test/Principal.java
@@ -5,5 +5,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Primer cambio en master");
 		System.out.println("Segundo cambio en master");
+		System.out.println("Tercer cambio en branch[second]");
 	}
 }
