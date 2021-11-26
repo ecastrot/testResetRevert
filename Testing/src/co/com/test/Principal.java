1cd1bac74a74b87cbe1ef91d5f9aae466d8bf4d2
Edison Castro <ecastrot@loggro.com>
2021-11-26 15:43:29 -0500

    Nueva linea 2

--- Testing/src/co/com/test/Principal.java
+++ Testing/src/co/com/test/Principal.java
@@ -23,5 +23,6 @@ public class Principal {
 		// -------------------------------------
 		System.out.println("Nueva linea 1");
 		System.out.println("1 + 1 = 2");
+		System.out.println("Nueva linea 2");
 	}
 }
